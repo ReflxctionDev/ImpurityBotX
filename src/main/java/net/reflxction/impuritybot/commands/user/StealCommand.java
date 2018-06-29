0d5ae5689e0cd1ce18a49eb504b3cdaf95f7c91d
BuildTools <unconfigured@null.spigotmc.org>
2018-06-29 18:34:38 +0300

    Added Steal and StealCommand and registered StealCommand

---
+++ src/main/java/net/reflxction/impuritybot/commands/user/StealCommand.java
@@ -0,0 +1,71 @@
+package net.reflxction.impuritybot.commands.user;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.reflxction.impuritybot.core.commands.AbstractCommand;
+import net.reflxction.impuritybot.core.commands.CommandCategory;
+import net.reflxction.impuritybot.core.listeners.Steal;
+import net.reflxction.impuritybot.utils.lang.StringUtils;
+
+public class StealCommand extends AbstractCommand {
+
+    @Override
+    public void process(JDA jda, Guild guild, Message message, MessageChannel channel, User user, String[] args) {
+        if (args.length < 2) {
+            channel.sendMessage("**Invalid usage!** Try " + getUsage()).queue();
+            return;
+        }
+        User target;
+        User executor = user;
+        int count;
+        try {
+            String id = StringUtils.mentionToId(args[0]);
+            target = jda.getUserById(id);
+        } catch (Exception e) {
+            channel.sendMessage("**Expected a user mention (or id), but found** `" + args[0] + "`**.**").queue();
+            return;
+        }
+        try {
+            count = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            channel.sendMessage("**Expected a number, but found** `" + args[1] + "`**.**").queue();
+            return;
+        }
+        Steal steal = new Steal();
+        steal.steal(guild.getMember(target), guild.getMember(executor), count, channel);
+    }
+
+    @Override
+    public String getCommand() {
+        return "steal";
+    }
+
+    @Override
+    public long getDelay() {
+        return 0;
+    }
+
+    @Override
+    public String getUsage() {
+        return "-steal <@user> <credits>";
+    }
+
+    @Override
+    public String getDescription() {
+        return "20% chance to steal at most of 20% of the target user's" +
+                " credits";
+    }
+
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.EROS;
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[0];
+    }
+}
---
+++ src/main/java/net/reflxction/impuritybot/core/listeners/Steal.java
@@ -0,0 +1,108 @@
+package net.reflxction.impuritybot.core.listeners;
+
+import me.brokenearth.core.scheduler.Timer;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.Role;
+import net.reflxction.impuritybot.utils.GuildUtils;
+import net.reflxction.impuritybot.utils.data.CreditsManager;
+import java.util.Random;
+
+/**
+ * @author BrokenEarth // BrokenEarthDev
+ * @version 1.0
+ * @see net.reflxction.impuritybot.commands.user.StealCommand
+ */
+public class Steal {
+    /**
+     * The credits manager instance
+     */
+    private final CreditsManager manager = new CreditsManager();
+
+    private int percentage;
+    private boolean isSuccessful = false;
+
+    public Steal() {
+        this.generate();
+    }
+
+    /**
+     * Generates a random number with a max of 100
+     */
+    private void generate() {
+        percentage = new Random().nextInt(100) + 1;
+    }
+    /**
+     * Attempts to steal 20% of the user's credits
+     */
+    public void steal(Member target, Member executor, int amount, MessageChannel channel) {
+        if (target == executor) {
+            if (channel != null) {
+                channel.sendMessage("You can't steal credits from yourself").queue();
+            }
+        }
+        if (channel != null) {
+            channel.sendMessage("Percentage: **" + getPercentage() + "**").queue();
+        }
+        if (getPercentage() < 80) {
+            if (channel != null) channel.sendMessage("**Oh no!** You were unlucky and ended up with less than 80%").queue();
+            mute(executor, target);
+            return;
+        }
+        if (amount > manager.getUserCredits(target.getUser())) {
+            if (channel != null)
+                channel.sendMessage("You are trying to steal more credits than **" + target.getUser().getName() + "** has").queue();
+            return;
+        }
+        if (amount < 1) {
+            if (channel != null)
+                channel.sendMessage("You can't steal less than **1** credit").queue();
+            return;
+        }
+        int percentage = (amount / manager.getUserCredits(target.getUser()) ) * 100;
+        if (percentage > 20) {
+            if (channel != null)
+                channel.sendMessage("You are trying to steal more than 20% of **" + target.getUser().getName() + "'s** credits").queue();
+            return;
+        }
+        isSuccessful = true;
+        manager.setUserCredits(target.getUser(), manager.getUserCredits(target.getUser()) - amount);
+        manager.setUserCredits(executor.getUser(), manager.getUserCredits(executor.getUser()) + amount);
+        if (channel != null) channel.sendMessage("You have stolen **" + amount + "** credits from **" + target.getUser().getName() + "**").queue();
+        mute(executor, target);
+    }
+    /**
+     * Mutes the user who stole the credits for 5 minutes
+     */
+    private void mute(Member executor, Member target) {
+        if (!isSuccessful) return;
+        Role muted = GuildUtils.guild().getRolesByName("Muted", true).get(0);
+        GuildUtils.controller().addRolesToMember(executor, muted).queue();
+        new Timer() {
+            @Override
+            public void run() {
+                GuildUtils.controller().removeRolesFromMember(target, muted).queue();
+            }
+        }.schedule(1000 * 60 * 5);
+    }
+    /**
+     * Defines if the user did steal or not
+     * @return it
+     */
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+    /**
+     * @return the percentage with a percent sign on front of it
+     */
+    public String getPercentageAsString() {
+        return percentage + "%";
+    }
+
+    /**
+     * @return the percentage
+     */
+    public int getPercentage() {
+        return percentage;
+    }
+}
