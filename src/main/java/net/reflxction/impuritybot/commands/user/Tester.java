073c56fbd8ca3b2c097c9368103080997d6758e0
BuildTools <aloosh.kasasbeh@gmail.com>
2018-06-30 22:49:03 +0300

    Created Tester.java

---
+++ src/main/java/net/reflxction/impuritybot/commands/user/Tester.java
@@ -0,0 +1,77 @@
+/*
+ * * Copyright 2018 github.com/ReflxctionDev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.reflxction.impuritybot.commands.user;
+
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.User;
+import net.reflxction.impuritybot.core.commands.AbstractCommand;
+import net.reflxction.impuritybot.core.commands.CommandCategory;
+import net.reflxction.impuritybot.core.others.Roles;
+
+/**
+ * Gives the user the tester role
+ */
+public class Tester extends AbstractCommand {
+
+    @Override
+    public String getCommand() {
+        return "tester";
+    }
+
+    @Override
+    public void process(JDA j, Guild g, Message m, MessageChannel c, User u, String[] args) {
+        switch (args.length) {
+            case 0:
+                g.getController().addSingleRoleToMember(g.getMember(u), Roles.TESTER).queue();
+                m.addReaction("\uD83D\uDC4C").queue();
+                break;
+            case 1:
+                if (args[0].equalsIgnoreCase("remove")) {
+                    g.getController().removeSingleRoleFromMember(g.getMember(u), Roles.TESTER).queue();
+                    m.addReaction("\uD83D\uDC4C").queue();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public String[] getAliases() {
+        return new String[0];
+    }
+
+    @Override
+    public CommandCategory getCategory() {
+        return CommandCategory.USER;
+    }
+
+    @Override
+    public long getDelay() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Receive the Tester role";
+    }
+
+    @Override
+    public String getUsage() {
+        return "-tester / -tester remove";
+    }
+}
