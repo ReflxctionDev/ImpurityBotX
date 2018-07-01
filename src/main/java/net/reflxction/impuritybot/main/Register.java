7f02f407c0e5ad51107b8f0a524eedbc7b5f5773
BuildTools <aloosh.kasasbeh@gmail.com>
2018-07-02 01:59:46 +0300

    Replaced Command array with the builder form

--- src/main/java/net/reflxction/impuritybot/main/Register.java
+++ src/main/java/net/reflxction/impuritybot/main/Register.java
@@ -22,9 +22,9 @@ import net.reflxction.impuritybot.commands.admin.news.*;
 import net.reflxction.impuritybot.commands.admin.roles.UpdateRoles;
 import net.reflxction.impuritybot.commands.admin.user.*;
 import net.reflxction.impuritybot.commands.fun.exclusive.EightBall;
-import net.reflxction.impuritybot.commands.hypixel.IGN;
-import net.reflxction.impuritybot.commands.hypixel.IGNOf;
-import net.reflxction.impuritybot.commands.hypixel.PlayerIGN;
+import net.reflxction.impuritybot.commands.minecraft.IGN;
+import net.reflxction.impuritybot.commands.minecraft.IGNOf;
+import net.reflxction.impuritybot.commands.minecraft.PlayerIGN;
 import net.reflxction.impuritybot.commands.level.LevelCmd;
 import net.reflxction.impuritybot.commands.level.RankCmd;
 import net.reflxction.impuritybot.commands.level.SetLevel;
@@ -33,9 +33,8 @@ import net.reflxction.impuritybot.commands.miscs.*;
 import net.reflxction.impuritybot.commands.points.ManagePointsCmd;
 import net.reflxction.impuritybot.commands.points.PointsBalance;
 import net.reflxction.impuritybot.commands.user.*;
-import net.reflxction.impuritybot.core.commands.AbstractCommand;
+import net.reflxction.impuritybot.core.commands.RegistryBuilder;
 import net.reflxction.impuritybot.core.eros.Slots;
-import net.reflxction.impuritybot.core.loggers.Logger;
 import net.reflxction.impuritybot.eros.*;
 import net.reflxction.impuritybot.eros.shop.Menu;
 import net.reflxction.impuritybot.logs.channel.ChannelCreatedLogger;
@@ -43,7 +42,6 @@ import net.reflxction.impuritybot.logs.channel.ChannelDeletedLogger;
 import net.reflxction.impuritybot.logs.message.MessageEditedLogger;
 import net.reflxction.impuritybot.logs.roles.RoleCreatedLogger;
 import net.reflxction.impuritybot.logs.roles.RoleDeletedLogger;
-import net.reflxction.impuritybot.logs.roles.RoleGivenLogger;
 import net.reflxction.impuritybot.logs.roles.RoleRemovedLogger;
 import net.reflxction.impuritybot.logs.server.ServerChangeNameLogger;
 import net.reflxction.impuritybot.logs.user.UserBanLogger;
@@ -51,36 +49,95 @@ import net.reflxction.impuritybot.logs.user.UserJoinLogger;
 import net.reflxction.impuritybot.logs.user.UserLeaveLogger;
 import net.reflxction.impuritybot.logs.user.UserNickLogger;
 
-import static net.reflxction.impuritybot.main.ImpurityBot.bot;
+import static net.reflxction.impuritybot.main.ImpurityBot.getBot;
 
 /**
  * Registry class
  *
- * @author BrokenEarth
+ * @author BrokenEarth & Reflxction
  */
 class Register {
 
-    private static AbstractCommand[] abstractCommands = {new Announce(), new About(), new Delete(), new EmbedMaker(), new GuildInfo(),
-            new HateMe(), new Help(), new HistoryDelete(), new Introduce(), new Kick(), new NewYear(), new Say(), new IGN(), new IGNOf(),
-            new RankCmd(), new LevelCmd(), new Pin(), new CommandInfo(), new EightBall(), new TopLevels(), new Daily(),
-            new PunishmentRules(), new Unpin(), new Warn(bot), new UserWarnings(bot), new RemoveWarn(bot), new ClearWarns(bot), new WarnInfo(bot),
-            new UpdateRoles(), new net.reflxction.impuritybot.commands.user.Invite(), new Ping(), new UserInfo(), new DiscordInfo(), new Notify(), new NotifyUpdate(), new Poll(bot),
-            new ToDo(), new ReleasedCommand(), new Balance(), new WelcomeManager(bot), new PlayerIGN(), new Menu(), new Buy(), new Credits(),
-            new Transfer(), new Open(), new Agree(), new Accept(), new Slots(), new SetLevel(), new Calendar(), new MakeCalendar(), new PointsBalance(), new ManagePointsCmd(),
-            new Unmute(), new Mute(), new MuteRemake(), new StealCommand(), new Tester(), new BetCommand(), new BetAmount()};
+    private RegistryBuilder builder = new RegistryBuilder(ImpurityBot.getJDA());
 
-    private static Logger[] loggers = {new UserJoinLogger(), new UserLeaveLogger(), new ChannelCreatedLogger(), new ChannelDeletedLogger(),
-            new MessageEditedLogger(), new UserBanLogger(), new UserNickLogger(), new ServerChangeNameLogger(), new RoleGivenLogger(), new RoleRemovedLogger(),
-            new RoleRemovedLogger(), new RoleCreatedLogger(), new RoleDeletedLogger()};
-
-    static AbstractCommand[] getAbstractCommands() {
-        return abstractCommands;
+    void registerCommands() {
+        builder
+                .registerCommand(new Announce())
+                .registerCommand(new About())
+                .registerCommand(new Delete())
+                .registerCommand(new MakeEmbed())
+                .registerCommand(new GuildInfo())
+                .registerCommand(new HateMe())
+                .registerCommand(new Help())
+                .registerCommand(new HistoryDelete())
+                .registerCommand(new Introduce())
+                .registerCommand(new Kick())
+                .registerCommand(new NewYear())
+                .registerCommand(new Say())
+                .registerCommand(new IGN())
+                .registerCommand(new IGNOf())
+                .registerCommand(new RankCmd())
+                .registerCommand(new LevelCmd())
+                .registerCommand(new Pin())
+                .registerCommand(new CommandInfo())
+                .registerCommand(new EightBall())
+                .registerCommand(new TopLevels())
+                .registerCommand(new Daily())
+                .registerCommand(new PunishmentRules())
+                .registerCommand(new Unpin())
+                .registerCommand(new Warn(getBot()))
+                .registerCommand(new UserWarnings(getBot()))
+                .registerCommand(new RemoveWarn(getBot()))
+                .registerCommand(new ClearWarns(getBot()))
+                .registerCommand(new WarnInfo(getBot()))
+                .disableCommand(new UpdateRoles())
+                .registerCommand(new Invite())
+                .registerCommand(new Ping())
+                .registerCommand(new UserInfo())
+                .registerCommand(new DiscordInfo())
+                .registerCommand(new Notify())
+                .registerCommand(new Poll(getBot()))
+                .registerCommand(new ToDo())
+                .registerCommand(new ReleasedCommand())
+                .registerCommand(new Balance())
+                .registerCommand(new WelcomeManager(getBot()))
+                .registerCommand(new PlayerIGN())
+                .registerCommand(new Menu())
+                .registerCommand(new Buy())
+                .registerCommand(new Credits())
+                .registerCommand(new Transfer())
+                .registerCommand(new Open())
+                .registerCommand(new Agree())
+                .registerCommand(new Accept())
+                .registerCommand(new Slots())
+                .registerCommand(new SetLevel())
+                .registerCommand(new Calendar())
+                .registerCommand(new MakeCalendar())
+                .registerCommand(new PointsBalance())
+                .registerCommand(new ManagePointsCmd())
+                .registerCommand(new Unmute())
+                .registerCommand(new Mute())
+                .registerCommand(new MuteRemake())
+                .registerCommand(new StealCommand())
+                .registerCommand(new Tester())
+                .disableCommand(new BetCommand())
+                .registerCommand(new BetAmount());
     }
 
-    static Logger[] getLoggers() {
-        return loggers;
+    void registerLoggers() {
+        builder
+                .registerLogger(new UserJoinLogger())
+                .registerLogger(new UserLeaveLogger())
+                .registerLogger(new ChannelCreatedLogger())
+                .registerLogger(new ChannelDeletedLogger())
+                .registerLogger(new MessageEditedLogger())
+                .registerLogger(new UserBanLogger())
+                .registerLogger(new UserNickLogger())
+                .registerLogger(new ServerChangeNameLogger())
+                .registerLogger(new RoleRemovedLogger())
+                .registerLogger(new RoleCreatedLogger())
+                .registerLogger(new RoleDeletedLogger());
     }
-
 }
 
 
