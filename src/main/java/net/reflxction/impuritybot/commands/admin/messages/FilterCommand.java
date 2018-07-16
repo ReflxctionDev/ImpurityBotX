522bc36bfc3841435df095022a69bf128411c27f
BuildTools <unconfigured@null.spigotmc.org>
2018-07-16 21:19:22 +0300

    Added perms check

--- src/main/java/net/reflxction/impuritybot/commands/admin/messages/FilterCommand.java
+++ src/main/java/net/reflxction/impuritybot/commands/admin/messages/FilterCommand.java
@@ -1,6 +1,7 @@
 package net.reflxction.impuritybot.commands.admin.messages;
 
 import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
@@ -8,6 +9,7 @@ import net.dv8tion.jda.core.entities.User;
 import net.reflxction.impuritybot.core.commands.AbstractCommand;
 import net.reflxction.impuritybot.core.commands.CommandCategory;
 import net.reflxction.impuritybot.core.listeners.Filter;
+import net.reflxction.impuritybot.core.others.Roles;
 
 public class FilterCommand extends AbstractCommand {
 
@@ -19,31 +21,35 @@ public class FilterCommand extends AbstractCommand {
 
     @Override
     public void process(JDA jda, Guild guild, Message message, MessageChannel channel, User user, String[] args) {
-        if (args.length == 0) {
-            String msg;
-            if (new Filter().filterOn()) msg = "on";
-            else msg = "off";
-            channel.sendMessage("The filter is currently **" + msg + "**").queue();
-        } else if (args.length == 1) {
-            if (args[0].equalsIgnoreCase("off") || args[0].equalsIgnoreCase("false")) {
-                if (new Filter().filterOn())
-                new Filter().setFilterStatus(false);
-                else {
-                    channel.sendMessage("The filter is already **off**").queue();
+        if (!guild.getMember(user).hasPermission(Permission.ADMINISTRATOR) && !(guild.getMember(user).getRoles().get(0).getPositionRaw() >= Roles.ADMIN.getPositionRaw())) {
+            channel.sendMessage("**Invalid permission!** You need to be admin or higher to use this command").queue();
+        } else {
+            if (args.length == 0) {
+                String msg;
+                if (new Filter().filterOn()) msg = "on";
+                else msg = "off";
+                channel.sendMessage("The filter is currently **" + msg + "**").queue();
+            } else if (args.length == 1) {
+                if (args[0].equalsIgnoreCase("off") || args[0].equalsIgnoreCase("false")) {
+                    if (new Filter().filterOn())
+                        new Filter().setFilterStatus(false);
+                    else {
+                        channel.sendMessage("The filter is already **off**").queue();
+                        return;
+                    }
+                } else if (args[0].equalsIgnoreCase("on") || args[0].equalsIgnoreCase("true")) {
+                    if (!new Filter().filterOn())
+                        new Filter().setFilterStatus(true);
+                    else {
+                        channel.sendMessage("The filter is already **on**").queue();
+                        return;
+                    }
+                } else {
+                    channel.sendMessage("Use **on** to filter messages or **off** not filter messages").queue();
                     return;
                 }
-            } else if (args[0].equalsIgnoreCase("on") || args[0].equalsIgnoreCase("true")) {
-                if (!new Filter().filterOn())
-                new Filter().setFilterStatus(true);
-                else {
-                    channel.sendMessage("The filter is already **on**").queue();
-                    return;
-                }
-            } else {
-                channel.sendMessage("Use **on** to filter messages or **off** not filter messages").queue();
-                return;
-            }
-        } else channel.sendMessage("**Invalid usage!** Use " + getUsage()).queue();
+            } else channel.sendMessage("**Invalid usage!** Use " + getUsage()).queue();
+        }
     }
 
     @Override
